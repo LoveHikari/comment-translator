60e61c1d6f7ec54eb4b9ba150623193333861a6e
LoveHikari <xmh.yxw@gmail.com>
2024-07-05 14:14:13 +0800

    更新

---
+++ QuickInfoTest1/TestQuickInfoSource.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
+
+namespace QuickInfoTest
+{
+    internal class TestQuickInfoSource : IQuickInfoSource
+    {
+        private TestQuickInfoSourceProvider m_provider;
+        private ITextBuffer m_subjectBuffer;
+        private Dictionary<string, string> m_dictionary;
+        private bool m_isDisposed;
+        internal TestQuickInfoSource(TestQuickInfoSourceProvider provider, ITextBuffer subjectBuffer)
+        {
+            m_provider = provider;
+            m_subjectBuffer = subjectBuffer;
+
+            //these are the method names and their descriptions
+            m_dictionary = new Dictionary<string, string>();
+            m_dictionary.Add("add", "int add(int firstInt, int secondInt)\nAdds one integer to another.");
+            m_dictionary.Add("subtract", "int subtract(int firstInt, int secondInt)\nSubtracts one integer from another.");
+            m_dictionary.Add("multiply", "int multiply(int firstInt, int secondInt)\nMultiplies one integer by another.");
+            m_dictionary.Add("divide", "int divide(int firstInt, int secondInt)\nDivides one integer by another.");
+        }
+        public void Dispose()
+        {
+            if (!m_isDisposed)
+            {
+                GC.SuppressFinalize(this);
+                m_isDisposed = true;
+            }
+        }
+
+        public void AugmentQuickInfoSession(IQuickInfoSession session, IList<object> quickInfoContent, out ITrackingSpan applicableToSpan)
+        {
+            // Map the trigger point down to our buffer.
+            SnapshotPoint? subjectTriggerPoint = session.GetTriggerPoint(m_subjectBuffer.CurrentSnapshot);
+            if (!subjectTriggerPoint.HasValue)
+            {
+                applicableToSpan = null;
+                return;
+            }
+
+            ITextSnapshot currentSnapshot = subjectTriggerPoint.Value.Snapshot;
+            SnapshotSpan querySpan = new SnapshotSpan(subjectTriggerPoint.Value, 0);
+
+            //look for occurrences of our QuickInfo words in the span
+            ITextStructureNavigator navigator = m_provider.NavigatorService.GetTextStructureNavigator(m_subjectBuffer);
+            TextExtent extent = navigator.GetExtentOfWord(subjectTriggerPoint.Value);
+            string searchText = extent.Span.GetText();
+
+            foreach (string key in m_dictionary.Keys)
+            {
+                int foundIndex = searchText.IndexOf(key, StringComparison.CurrentCultureIgnoreCase);
+                if (foundIndex > -1)
+                {
+                    applicableToSpan = currentSnapshot.CreateTrackingSpan
+                        (
+                                                //querySpan.Start.Add(foundIndex).Position, 9, SpanTrackingMode.EdgeInclusive
+                                                extent.Span.Start + foundIndex, key.Length, SpanTrackingMode.EdgeInclusive
+                        );
+
+                    string value;
+                    m_dictionary.TryGetValue(key, out value);
+                    if (value != null)
+                        quickInfoContent.Add(value);
+                    else
+                        quickInfoContent.Add("");
+
+                    return;
+                }
+            }
+
+            applicableToSpan = null;
+        }
+    }
+}
